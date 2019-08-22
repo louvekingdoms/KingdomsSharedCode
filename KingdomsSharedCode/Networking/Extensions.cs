4ceb73aea299351b5e2dbf893e4ea6fa93f157c4
rackover <rackover@live.fr>
2019-08-23 00:16:23 +0530

    Transferred message writing to the message class

--- KingdomsSharedCode/Networking/Extensions.cs
+++ KingdomsSharedCode/Networking/Extensions.cs
@@ -17,20 +17,7 @@ namespace KingdomsSharedCode.Networking
 
         public static void Write(this NetworkStream stream, Message message)
         {
-            // Can throw SocketException and IOException
-            if (!stream.CanWrite)
-            {
-                throw new Exception("Expected to be able to write into the network stream, but could not (canWrite is false)");
-            }
-            using (var writer = new BinaryWriter(stream, Encoding.UTF8, leaveOpen: true))
-            {
-                writer.Write(message.controller);
-                writer.Write(message.beat);
-                writer.Write(message.sumAtBeat);
-                writer.Write(message.session);
-                writer.Write(message.secret);
-                writer.Write(message.body);
-            }
+            message.Write(stream);
         }
     }
 }
--- KingdomsSharedCode/Networking/Message.cs
+++ KingdomsSharedCode/Networking/Message.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,18 +12,18 @@ namespace KingdomsSharedCode.Networking
     {
         public byte controller;
         public ushort beat;
-        public byte[] sumAtBeat;
+        public byte[] sumAtBeat = new byte[16];// MD5 hash size is 32 bytes
         public uint session;
         public uint secret;
         public string body = string.Empty;
 
-        public Message(byte[] buffer, int bytesRead) : this(new BinaryReader(new MemoryStream(buffer), Encoding.UTF8)) {}
+        //public Message(byte[] buffer, int bytesRead) : this(new BinaryReader(new MemoryStream(buffer), Encoding.UTF8)) {}
 
         public Message(BinaryReader reader)
         {
             controller = reader.ReadByte();
             beat = reader.ReadUInt16();
-            sumAtBeat = reader.ReadBytes(32); // MD5 hash size is 32 bytes
+            sumAtBeat = reader.ReadBytes(sumAtBeat.Length);
             session = reader.ReadUInt32();
             secret = reader.ReadUInt32();
             body = reader.ReadString();
@@ -30,14 +31,28 @@ namespace KingdomsSharedCode.Networking
 
         public Message() { }
 
+        public void Write(NetworkStream stream)
+        {
+            using (var writer = new BinaryWriter(stream, Encoding.UTF8, leaveOpen: true))
+            {
+                var mes = this;
+                writer.Write(mes.controller);
+                writer.Write(mes.beat);
+                writer.Write(mes.sumAtBeat);
+                writer.Write(mes.session);
+                writer.Write(mes.secret);
+                writer.Write(mes.body);
+            }
+        }
+
         public override string ToString()
         {
-            return string.Format("[{0}  {1}  {2}  {3}  {4}]     {5}",
+            return string.Format("[{0}  BEAT:{1}  SES:{2}  SEC:{3}  SUM:{4}] [{5}]",
                 ((Controller)controller).ToString(),
                 beat.ToString("X4"),
                 session.ToString("X8"),
                 secret.ToString("X8"),
-                sumAtBeat[0].ToString("X2"),
+                string.Join(" ", sumAtBeat),
                 body
             );
         }
