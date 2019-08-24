4b014f4da1c3c7cf5c0a415d7e97d724468bd8c4
rackover <rackover@live.fr>
2019-08-24 18:58:08 +0530

    Added chat and desync controllers

--- KingdomsSharedCode/Networking/Controller.cs
+++ KingdomsSharedCode/Networking/Controller.cs
@@ -13,9 +13,11 @@ namespace KingdomsSharedCode.Networking
         RELAY_HEARTBEAT     =   4,
 
         // CLIENT
-        SESSION_INFO        =   5,      // 
+        SESSION_INFO        =   5,      // Contains session info and starting beat
         BROADCAST           =   6,      // Debug message to be broadcasted to every client of the same session
         WAIT                =   7,      // Client must pause its internal clock to wait for other clients
-        GO                  =   8       // Client can resume its internal clock
+        GO                  =   8,      // Client can resume its internal clock
+        DESYNCHRONIZED      =   9,      // Game state is desynchronized between clients, simulation should be interrupted.
+        CHAT                =   10      // Chat message. Similar to Broadcast
     }
 }
